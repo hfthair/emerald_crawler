4c9e93a74a8eb108ecb9d80395aa16f72bbaf010
Lei Zhang <zhangleihebut@gmail.com>
2021-02-09 20:23:30 -0500

    check which journal is oa/pitt/no_access

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Download public paper from emerald
 
+## Install requirements
+> pip install -r requirements.txt
+
+## Get cookies
+1. Login account and visit [a emerald paper link](https://www.emerald.com/insight/content/doi/10.1108/AAAJ-02-2019-3890/full/html) to check the fulltext
+2. Open developer tool of the browser: Application -> Cookies
+3. Copy all Key:Value pairs to `cookies.py`
+
 ## Download to csv
-> python download.py --save_dir . --pitt_auth False
+> python download.py --save_dir . --auth_by_cookie True
+
+### to skip open access and pitt access journals
+> python download.py --save_dir . --auth_by_cookie True --skip_open_access_and_pitt_authed True
+
 
 ## convert to jsonl
 > python csv2jsonl.py --csv_dir . --jsonl_filename emerald.jsonl
--- download.py
+++ download.py
@@ -11,6 +11,7 @@ import requests
 from cookies import cookies
 
 base_url = "https://www.emerald.com"
+authorized_url = "https://www-emerald-com.pitt.idm.oclc.org"
 
 with open('./ua.txt', 'r', encoding='utf8') as f:
     ualist = [i.strip() for i in f]
@@ -20,8 +21,8 @@ def randua():
     return {'headers': random.choice(ualist)}
 
 
-def download_full_via_pitt(url):
-    url = url.replace(base_url, "https://www-emerald-com.pitt.idm.oclc.org")
+def download_full_with_cookie(url):
+    url = url.replace(base_url, authorized_url)
     r = None
     for delay in (1, 5, 10, 15):
         try:
@@ -48,107 +49,185 @@ def get_retry(url):
     return r
 
 
-def main(save_dir, pitt_auth=False):
+def check_journal_access(journal_url):
+    jpage = get_retry(journal_url)
+
+    jpage_entries = re.findall(
+        r'<a href="/insight/publication.*class="intent_tocIssueLink".*</a>', jpage.text)
+    try:
+        for entry in jpage_entries:
+            entry_url = entry.replace('<a href="', '')
+            entry_url = base_url + entry_url[:entry_url.index('"')]
+            article_list_page = get_retry(entry_url)
+
+            art_urls = re.findall(
+                r'href="/insight/content.*full/html', article_list_page.text)
+            art_urls = [base_url + i[6:] for i in art_urls]
+
+            for art_url in art_urls:
+                time.sleep(random.random() * 1.18)
+
+                art_page_public = get_retry(art_url)
+                soup = bs4.BeautifulSoup(art_page_public.text, 'html.parser')
+
+                abstract = soup.find('div', {'class': 'Abstract__block'})
+                if not abstract:
+                    continue
+
+                if 'To read the full version of this content please select one of the options below' in art_page_public.text:
+                    full = download_full_with_cookie(art_url)
+                    return 'pitt_access'
+                else:
+                    return 'open_access'
+
+    except FileNotFoundError as e:
+        return 'no_access'
+    return 'error'
+
+def check_access():
     r = get_retry("https://www.emerald.com/insight/sitemap/publications")
-    found = re.findall(r'<a href="/insight/publication.*</a>', r.text)
-    journal_url_list = []
-    for i in found:
-        url = i.replace('<a href="', '')
-        url = base_url + url[:35]
-        journal_url_list.append(url)
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    journal_section = soup.find('section', {'id': 'journals'})
+    journal_url_list = [base_url+a['href'] for a in journal_section.find_all('a')]
 
-    for nth_jour, j_page_url in enumerate(journal_url_list):
-        if os.path.isfile(os.path.join(save_dir, f'out{nth_jour+1}.csv')):
-            # resume from last point...
-            print(f"skip {nth_jour} ...")
-            continue
-        fname = os.path.join(save_dir, f'out{nth_jour}.csv')
-        if os.path.isfile(fname) and os.path.getsize(fname) > 100:
-            df = pd.read_csv(fname, encoding='utf8')
-        else:
-            df = pd.DataFrame()
+    availability = {}
+    for nth_journal, journal_url in enumerate(journal_url_list):
+        issn = journal_url.split('/')[-1]
+        print(f'trying issn-{issn} ({nth_journal}) ...', end='')
+        avail = check_journal_access(journal_url)
+        availability[issn] = avail
+        print(f'.........{avail}')
 
-        jpage = get_retry(j_page_url)
+    from pprint import pprint
+    import json
+    pprint(availability)
+    json.dump(availability, open('availability.json', 'w'))
 
-        jpage_entries = re.findall(
-            r'<a href="/insight/publication.*class="intent_tocIssueLink".*</a>', jpage.text)
-        try:
-            for entry in jpage_entries:
-                entry_url = entry.replace('<a href="', '')
-                entry_url = base_url + entry_url[:entry_url.index('"')]
+    return availability
 
-                article_list_page = get_retry(entry_url)
+def download_journal(journal_url, fname, auth_by_cookie):
+    if os.path.isfile(fname) and os.path.getsize(fname) > 100:
+        df = pd.read_csv(fname, encoding='utf8')
+    else:
+        df = pd.DataFrame()
 
-                art_urls = re.findall(
-                    r'href="/insight/content.*full/html', article_list_page.text)
-                art_urls = [base_url + i[6:] for i in art_urls]
+    jpage = get_retry(journal_url)
 
-                for art_url in art_urls:
-                    if df.shape[0] > 0 and art_url in set(df.url.to_list()): # df[df.url == purl].shape[0] > 0:
-                        print(" # already done.")
-                        continue
-                    time.sleep(random.random() * 1.18)
+    jpage_entries = re.findall(
+        r'<a href="/insight/publication.*class="intent_tocIssueLink".*</a>', jpage.text)
+    try:
+        for entry in jpage_entries:
+            entry_url = entry.replace('<a href="', '')
+            entry_url = base_url + entry_url[:entry_url.index('"')]
 
-                    art_page_public = get_retry(art_url)
-                    soup = bs4.BeautifulSoup(art_page_public.text, 'html.parser')
+            article_list_page = get_retry(entry_url)
 
-                    row = {'url': art_url}
+            art_urls = re.findall(
+                r'href="/insight/content.*full/html', article_list_page.text)
+            art_urls = [base_url + i[6:] for i in art_urls]
 
-                    abstract = soup.find('div', {'class': 'Abstract__block'})
-                    if not abstract:
-                        continue
-                    row['abstract'] = abstract.text.strip()
+            for art_url in art_urls:
+                if df.shape[0] > 0 and art_url in set(df.url.to_list()): # df[df.url == purl].shape[0] > 0:
+                    print(" # already done.")
+                    continue
+                time.sleep(random.random() * 1.18)
 
-                    title = soup.find('h1', {'class': 'intent_article_title'})
-                    row['title'] = ''
-                    if title:
-                        row['title'] = title.text.strip()
+                art_page_public = get_retry(art_url)
+                soup = bs4.BeautifulSoup(art_page_public.text, 'html.parser')
 
-                    authors = soup.find(
-                        'section', {'id': 'intent_contributors'})
-                    row['authors'] = ''
-                    if authors:
-                        row['authors'] = authors.text.strip()
+                row = {'url': art_url}
 
-                    keyul = soup.find(
-                        'ul', {'aria-labelledby': 'page__keywords-label'})
-                    row['keywords'] = ''
-                    if keyul:
-                        row['keywords'] = ([i.text.strip()
-                                            for i in keyul.findAll('li')])
+                abstract = soup.find('div', {'class': 'Abstract__block'})
+                if not abstract:
+                    continue
+                row['abstract'] = abstract.text.strip()
 
-                    breadcrumbs = soup.find('breadcrumbs')
-                    row['path'] = ''
-                    if breadcrumbs:
-                        row['path'] = '/'.join(i[0]
-                                               for i in eval(breadcrumbs.attrs['crumbs']) if i)
+                title = soup.find('h1', {'class': 'intent_article_title'})
+                row['title'] = ''
+                if title:
+                    row['title'] = title.text.strip()
 
-                    print(f" # trying ({nth_jour}) {row['path']}...")
+                authors = soup.find(
+                    'section', {'id': 'intent_contributors'})
+                row['authors'] = ''
+                if authors:
+                    row['authors'] = authors.text.strip()
 
-                    if 'To read the full version of this content please select one of the options below' in art_page_public.text:
-                        if pitt_auth:
-                            full = download_full_via_pitt(art_url)
-                            row['full_html'] = full
-                        else:
-                            raise FileNotFoundError('Journal is not publicly accessable.')
+                keyul = soup.find(
+                    'ul', {'aria-labelledby': 'page__keywords-label'})
+                row['keywords'] = ''
+                if keyul:
+                    row['keywords'] = ([i.text.strip()
+                                        for i in keyul.findAll('li')])
+
+                breadcrumbs = soup.find('breadcrumbs')
+                row['path'] = ''
+                if breadcrumbs:
+                    row['path'] = '/'.join(i[0]
+                                            for i in eval(breadcrumbs.attrs['crumbs']) if i)
+
+                print(f" # trying {row['path']}...")
+
+                if 'To read the full version of this content please select one of the options below' in art_page_public.text:
+                    if auth_by_cookie:
+                        full = download_full_with_cookie(art_url)
+                        row['full_html'] = full
                     else:
-                        row['full_html'] = art_page_public.text
-                    row['short_html'] = art_page_public.text
+                        raise FileNotFoundError('Journal is not publicly accessable.')
+                else:
+                    row['full_html'] = art_page_public.text
+                row['short_html'] = art_page_public.text
 
-                    df = df.append(row, ignore_index=True)
+                df = df.append(row, ignore_index=True)
 
-                    print(f"  | done ({df.shape[0]})")
+                print(f"  | done ({df.shape[0]})")
 
-                    if df.shape[0] % 100 == 0:
-                        df.to_csv(fname, encoding='utf8', index=False)
+                if df.shape[0] % 100 == 0:
+                    df.to_csv(fname, encoding='utf8', index=False)
 
-        except FileNotFoundError as e:
-            print("  ", e)
+    except FileNotFoundError as e:
+        print("  ", e)
+    if df.shape[0] > 0:
         df.to_csv(fname, encoding='utf8', index=False)
-    return nth_jour
+
+
+def main(save_dir, auth_by_cookie=True, skip_open_access_and_pitt_authed=False):
+    r = get_retry("https://www.emerald.com/insight/sitemap/publications")
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    journal_section = soup.find('section', {'id': 'journals'})
+    journal_url_list = [base_url+a['href'] for a in journal_section.find_all('a')]
+
+    skip_issn = set()
+    if skip_open_access_and_pitt_authed:
+        # skip open access and pitt authed journals
+        import json
+        availability = json.load(open('availability.json', 'r'))
+        skip_issn = {issn for issn in availability if availability[issn] != 'no_access'}
+
+    # find current progress to resume
+    current_progress = 0
+    for nth_journal, journal_url in enumerate(journal_url_list):
+        issn = journal_url.split('/')[-1]
+        if os.path.isfile(os.path.join(save_dir, f'issn-{issn}-({nth_journal}).csv')) or issn in skip_issn:
+            current_progress = nth_journal
+
+    for nth_journal, journal_url in enumerate(journal_url_list):
+        if nth_journal < current_progress:
+            print(f"skip {nth_journal}th journal ...")
+            continue
+
+        issn = journal_url.split('/')[-1]
+        if issn in skip_issn:
+            print(f"skip {nth_journal}th journal ...")
+            continue
+
+        fname = os.path.join(save_dir, f'issn-{issn}-({nth_journal}).csv')
+        download_journal(journal_url, fname, auth_by_cookie)
+
+    return nth_journal
 
 
 if __name__ == '__main__':
     import fire
-    # --save_dir dir --pitt_auth True/False(need to set cookies.py if True)
+    # --save_dir dir --auth_by_cookie True/False(need to set cookies.py if True)
     fire.Fire(main)
